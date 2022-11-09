7cb97ec694d612457ca14c641802a5a9d85d6e6f
LughsClues <cuberfreak@gmail.com>
2022-11-09 12:56:47 +0100

    Add initialize method

--- color_pegs.rb
+++ color_pegs.rb
@@ -1,38 +1,39 @@
 class String
     # colorization
-    def colorize(color_code)
-      "\e[#{color_code}m#{self}\e[0m"
-    end
-  
-    def red
-      colorize(31)
-    end
-  
-    def green
-      colorize(32)
-    end
-  
-    def yellow
-      colorize(33)
-    end
-  
-    def blue
-      colorize(34)
-    end
-  
-    def pink
-      colorize(35)
-    end
-  
-    def light_blue
-      colorize(36)
-    end
+  def black;          "\e[30m#{self}\e[0m" end
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def brown;          "\e[33m#{self}\e[0m" end
+  def blue;           "\e[34m#{self}\e[0m" end
+  def magenta;        "\e[35m#{self}\e[0m" end
+  def cyan;           "\e[36m#{self}\e[0m" end
+  def gray;           "\e[37m#{self}\e[0m" end
+
 end
   
 
 class ColorPegs
-    #How do I save color values?
+  attr_reader :red_peg
+  attr_reader :green_peg
+  attr_reader :brown_peg
+  attr_reader :blue_peg
+  attr_reader :magenta_peg
+  attr_reader :cyan_peg
+  attr_reader :gray_peg
+  attr_reader :black_peg
+
+  def initialize
+    @red_peg = '⬤'.red
+    @green_peg = '⬤'.green
+    @brown_peg = '⬤'.brown
+    @blue_peg = '⬤'.blue
+    @magenta_peg = '⬤'.magenta
+    @cyan_peg = '⬤'.cyan
+    @gray_peg = '⬤'.gray
+    @black_peg = '⬤'.black
+  end
+    
 end
 
-puts "this is a test".yellow 
-puts "another".green
+test = ColorPegs.new
+puts test.red_peg
