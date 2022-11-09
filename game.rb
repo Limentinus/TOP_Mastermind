f0362c4662fba87f2d6ae549356f85a717c75507
LughsClues <cuberfreak@gmail.com>
2022-11-09 17:20:28 +0100

    Improved formatting

--- game.rb
+++ game.rb
@@ -1,11 +1,11 @@
 class Game
-    def initialize(codemaker_class, codebreaker_class)
-        @codemaker = codemaker_class.new
-        @codebreaker = codebreaker_class.new
-        @board
-    end
+  def initialize(codemaker_class, codebreaker_class)
+    @codemaker = codemaker_class.new
+    @codebreaker = codebreaker_class.new
+    @code = []
+  end
 
-    def play_game
-        
-    end
+  def play_game
+    @codebreaker.
+  end
 end
