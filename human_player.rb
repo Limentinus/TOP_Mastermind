c3ddae56d283b12d9be4ac10d99d2ef30af1a649
LughsClues <cuberfreak@gmail.com>
2022-11-09 13:25:06 +0100

    add guess_code and make_code methods

--- human_player.rb
+++ human_player.rb
@@ -1,1 +1,26 @@
 class HumanPlayer
+  def initialize
+  end
+
+  def guess_code
+    array = Array.new(4)
+    guess = array.map do |color|
+        puts "Guess color"
+        gets.chomp
+    end
+    puts guess
+  end
+
+  def make_code
+    array = Array.new(4)
+    code = array.map do |color|
+        puts "Make the code"
+        gets.chomp
+    end
+    puts code
+  end
+end
+
+
+test_player = HumanPlayer.new
+test_player.guess_code
