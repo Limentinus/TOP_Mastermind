dff25d1c69afe69379f284a8912a1c0f51e4fbd1
LughsClues <cuberfreak@gmail.com>
2022-11-09 10:11:15 +0100

    Wrote initial print board method

---
+++ display.rb
@@ -0,0 +1,16 @@
+class Display
+
+  def initialize
+  end
+
+  def print_board(guesses, feedback)
+    puts "Guesses:  | #{guesses[0]} | #{guesses[1]} | #{guesses[2]} | #{guesses[3]} |"
+    puts "Feedback: | #{feedback[0]} | #{feedback[1]} | #{feedback[2]} | #{feedback[3]} |"
+  end
+end
+
+test_guesses = Array.new(4)
+test_feedback = Array.new(4)
+test = Display.new
+
+test.print_board(test_guesses, test_feedback)
