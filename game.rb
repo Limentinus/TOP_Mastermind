c473e05861c432753c4fe9ab485a19cec13d1311
LughsClues <cuberfreak@gmail.com>
2022-11-14 18:39:15 +0100

    Changed @color to a hash

--- game.rb
+++ game.rb
@@ -1,15 +1,35 @@
+require_relative 'display.rb'
+require_relative 'human_player.rb'
+require_relative 'computer_player.rb'
+
 class Game
+    attr_reader :code
+
   def initialize(codemaker_class, codebreaker_class)
     @codemaker = codemaker_class.new
     @codebreaker = codebreaker_class.new
-    @code = []
+    @code = @codemaker.make_code
   end
 
   def play_game
-    @codebreaker.
+    @codemaker.make_code
+    loop do
+        # print_board
+        guess = @codebreaker.guess_code
+        if won?(@codemaker.code, guess) 
+            puts "Congrats, you guessed the correct code: #{guess}"
+            break
+        elsif guess == "exit"
+            break
+        else puts "That wasn't the correct code, try again."
+        end
+    end
   end
 
   def won?(code, guess)
-    code == guess
+    puts code == guess
   end
 end
+
+test = Game.new(ComputerPlayer, HumanPlayer)
+test.print_board
