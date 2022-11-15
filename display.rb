ebc91412cae8ea4289bceb7e073184ee46d43f49
LughsClues <cuberfreak@gmail.com>
2022-11-15 12:54:16 +0100

    Add print board to play_game method

--- display.rb
+++ display.rb
@@ -33,6 +33,14 @@ module Display
   #   "\e[37m#{self}\e[0m"
   # end
 
+  def make_colors(color_codes, color_pegs)
+    color_array = []
+    color_codes.each do |code|
+      color_array << color_pegs[code]
+    end
+    return color_array
+  end
+
   def print_board(guesses, feedback)
     puts "Guesses:  | #{guesses[0]} | #{guesses[1]} | #{guesses[2]} | #{guesses[3]} |"
     puts "Feedback: | #{feedback[0]} | #{feedback[1]} | #{feedback[2]} | #{feedback[3]} |"
--- game.rb
+++ game.rb
@@ -1,25 +1,31 @@
 require_relative 'display.rb'
 require_relative 'human_player.rb'
 require_relative 'computer_player.rb'
+require_relative 'color_pegs.rb'
 
 class Game
+    include Display
     attr_reader :code
 
   def initialize(codemaker_class, codebreaker_class)
     @codemaker = codemaker_class.new
     @codebreaker = codebreaker_class.new
     @code = @codemaker.make_code
+    @colors = ColorPegs.new.colors
+    @guess = []
+    @feedback = []
   end
 
   def play_game
     @codemaker.make_code
     loop do
-        # print_board
-        guess = @codebreaker.guess_code
-        if won?(@codemaker.code, guess) 
-            puts "Congrats, you guessed the correct code: #{guess}"
+        @guess = @codebreaker.guess_code
+        guess_colors = make_colors(@guess, @colors)
+        print_board(guess_colors, @feedback)
+        if won?(@codemaker.code, @guess) 
+            puts "Congrats, you guessed the correct code: #{@guess}"
             break
-        elsif guess == "exit"
+        elsif @guess == ['e','x','i','t']
             break
         else puts "That wasn't the correct code, try again."
         end
@@ -32,4 +38,4 @@ class Game
 end
 
 test = Game.new(ComputerPlayer, HumanPlayer)
-test.print_board
+test.play_game
