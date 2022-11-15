e9025c0797a09e073f236f4905dbc0309e8cdc4a
LughsClues <cuberfreak@gmail.com>
2022-11-15 15:52:12 +0100

    corrected correct_colors method

--- game.rb
+++ game.rb
@@ -4,8 +4,8 @@ require_relative 'computer_player.rb'
 require_relative 'color_pegs.rb'
 
 class Game
-    include Display
-    attr_reader :code
+  include Display
+  attr_reader :code
 
   def initialize(codemaker_class, codebreaker_class)
     @codemaker = codemaker_class.new
@@ -19,20 +19,21 @@ class Game
   def play_game
     @codemaker.make_code
     loop do
-        @guess = @codebreaker.guess_code
-        guess_colors = make_colors(@guess, @colors)
-        print_board(guess_colors, @feedback)
-        puts correct_colors(@guess)
-        puts correct_place(@guess)
-        if won?
-            puts "Congrats, you guessed the correct code: #{@guess}"
-            puts @code
-            break
-        elsif @guess == ['e','x','i','t']
-            break
-        else puts "That wasn't the correct code, try again."
-             puts @code
-        end
+      @guess = @codebreaker.guess_code
+      guess_colors = make_colors(@guess, @colors)
+      print_board(guess_colors, @feedback)
+      puts correct_colors(@guess)
+      puts correct_place(@guess)
+      if won?
+        puts "Congrats, you guessed the correct code: #{@guess}"
+        puts @code
+        break
+      elsif @guess == ['e','x','i','t']
+        break
+      else
+        puts "That wasn't the correct code, try again."
+        puts @code
+      end
     end
   end
 
@@ -42,20 +43,19 @@ class Game
 
   def correct_colors(guess)
     # method determins if a color code is pressent in the @code and then counts it
-    @code.intersection(guess).size
+    4 - @code.difference(guess).size
   end
 
   def correct_place(guess)
     correct = 0
     @code.each_with_index do |letter, index|
-        if letter == guess[index]
-            correct += 1
-        end
+      if letter == guess[index]
+        correct += 1
+      end
     end
     return correct
   end
 
-        
 
 end
 
