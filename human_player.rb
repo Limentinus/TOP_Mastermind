1c132e3c8aa021ab002deeff194e753ea978b95c
LughsClues <cuberfreak@gmail.com>
2022-12-01 14:21:26 +0100

    Made guess_code method run on one line

--- human_player.rb
+++ human_player.rb
@@ -3,13 +3,10 @@ class HumanPlayer
   end
 
   def guess_code
-    array = Array.new(4)
-    guess = array.map do |color|
-        puts "Guess color"
-        puts "r: red, g: green, b: blue, y: yellow, m: magenta, c: cyan"
-        gets.chomp
-    end
-    return guess
+    puts "Guess 4 colors using the color codes. To exit the game type exit."
+    puts "r: red, g: green, b: blue, y: yellow, m: magenta, c: cyan"
+    guess = gets.chomp
+    return guess.split('')
   end
 
   def make_code
